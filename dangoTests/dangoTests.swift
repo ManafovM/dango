e4ef45bc0a68a18055050ecdc87b255910c5020f
marif.manafov <marif.manafov@aijus.com>
2024-12-01 12:16:14 +0900

    Initial Commit

---
+++ dangoTests/dangoTests.swift
@@ -0,0 +1,17 @@
+//
+//  dangoTests.swift
+//  dangoTests
+//
+//  Created by マナフォフ・マリフ on 2024/12/01.
+//
+
+import Testing
+@testable import dango
+
+struct dangoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
