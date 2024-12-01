523aa4a7e4527d7e55cade64f72b152d051e859f
marif.manafov <marif.manafov@aijus.com>
2024-12-01 12:57:34 +0900

    Storyboard のセットアップ

---
+++ dango/View Controllers/VideoCollectionViewController.swift
@@ -0,0 +1,40 @@
+//
+//  VideoCollectionViewController.swift
+//  dango
+//
+//  Created by マナフォフ・マリフ on 2024/12/01.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class VideoCollectionViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+    
+    // MARK: UICollectionViewDataSource
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 0
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        
+        // Configure the cell
+        
+        return cell
+    }
+}
--- dango/View Controllers/VideoDetailsViewController.swift
+++ dango/View Controllers/VideoDetailsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  VideoDetailsViewController.swift
 //  dango
 //
 //  Created by マナフォフ・マリフ on 2024/12/01.
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class VideoDetailsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
