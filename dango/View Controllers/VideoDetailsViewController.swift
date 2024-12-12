60311a67890b422883bae6d64e4f3a54c50f7040
marif.manafov <marif.manafov@aijus.com>
2024-12-12 17:41:53 +0900

    タイトルを追加

--- dango/View Controllers/VideoDetailsViewController.swift
+++ dango/View Controllers/VideoDetailsViewController.swift
@@ -14,6 +14,7 @@ class VideoDetailsViewController: BaseViewController, UIScrollViewDelegate {
     let topImage = FadeEdgeImageView()
     var topImageHeight: CGFloat!
     let scrollView = UIScrollView()
+    let contentView = UIView()
     let visualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .systemThickMaterialDark))
     
     let video: Video!
@@ -36,17 +37,39 @@ class VideoDetailsViewController: BaseViewController, UIScrollViewDelegate {
     }
     
     func setupView() {
-        setupScrollView()
         setupTopImageView()
+        setupScrollView()
+        setupContentView()
+        setupVideoInfo()
         
         view.bringSubviewToFront(closeButton)
     }
     
     func setupScrollView() {
         scrollView.frame = view.bounds
-        scrollView.contentSize = CGSize(width: view.frame.width, height: view.frame.height * 2)
         scrollView.delegate = self
         view.addSubview(scrollView)
+        
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        ])
+    }
+    
+    func setupContentView() {
+        scrollView.addSubview(contentView)
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            contentView.heightAnchor.constraint(equalToConstant: view.frame.height * 2) // TODO: Delete this
+        ])
     }
     
     func setupTopImageView() {
@@ -73,6 +96,44 @@ class VideoDetailsViewController: BaseViewController, UIScrollViewDelegate {
         visualEffectView.frame = topImage.bounds
         visualEffectView.alpha = 0
         topImage.addSubview(visualEffectView)
+    }
+    
+    func setupVideoInfo() {
+        let stackView: UIStackView = {
+            let stackView = UIStackView()
+            stackView.axis = .vertical
+            stackView.spacing = 0
+            stackView.distribution = .fill
+            stackView.alignment = .fill
+            
+            return stackView
+        }()
+        
+        let titleLabel: UILabel = {
+            let label = UILabel()
+            label.text = video.title
+            label.font = UIFont.systemFont(ofSize: 28, weight: .bold)
+            label.setContentHuggingPriority(.required, for: .vertical)
+            
+            return label
+        }()
+        
+        contentView.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: topImage.frame.height * 0.95),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16)
+        ])
+        
+        stackView.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: stackView.topAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: stackView.bottomAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: stackView.leadingAnchor)
+        ])
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
